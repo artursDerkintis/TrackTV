8cfd71037129bc38bfdba71c9d65d744fe41b8be
Arturs Derkintis <artursderkintis@gmail.com>
2017-04-23 22:03:18 +0300

    Created UI of MovieCollectionViewCell

---
+++ TrackTV/View/MovieCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCollectionViewCell.swift
+//  TrackTV
+//
+//  Created by Arturs Derkintis on 23/04/2017.
+//  Copyright © 2017 Arturs Derkintis. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+}
