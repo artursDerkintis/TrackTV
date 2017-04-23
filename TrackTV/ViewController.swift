1f4be4480193386ff3187865f438d9816cb8c91e
Arturs Derkintis <artursderkintis@gmail.com>
2017-04-23 21:06:38 +0300

    Initial Commit

---
+++ TrackTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrackTV
+//
+//  Created by Arturs Derkintis on 23/04/2017.
+//  Copyright © 2017 Arturs Derkintis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
